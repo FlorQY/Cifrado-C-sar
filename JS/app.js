013b50d74643ed6857f351fbe4e5c0046d44f4fc
Mily95 <flor_mili.95@hotmail.com>
2017-10-10 12:38:12 -0500

    subiendo avance

--- JS/app.js
+++ JS/app.js
@@ -1,20 +1,23 @@
-var sentence = prompt("ingrese una frase");
+var sentence = prompt("ingrese una frase");// creamos nuestro comando prompt
+var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" ; //declaramos nuestra variable alphabet cuyos elemntos son las letras del alfabeto.
+var position = alphabet.indexOf(alphabet[i]);
+var division1 = (position + 33)%52// declaramos la variable divison1 que nosa da el residuo de dicha division.
+var division2=  52*q +residue// declaramos la variable division2 en la que ingresamos los componentes de una division .
+ while(i < sentence.length)// un ciclo while para que procedimiento se repita hasta que culmine las letras de la frase
 
-var alphabet1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ;
-var alphabet2 = "abcdefghijklmnopqrstuvwxyz"
-while (i<frase.length){
-for(i=0;i<27;i++) {
+   for(i=0;i<52;i++) {//aqui comenzamos con for para que recorra toso los elementos de nuestra variable principal alphabet.
 
-if(alphabet1[i]==frase[i]){
-return i
-}
+       if(sentence[i] == alphabet[i]){//la condicional nos sirve para ver que  si la primera letra de la frase ingresada es igual a la letra en nuestra variable alphabet
+        return position;// si se cumple entonces nos retornará position que en si es la posicion de la primera letra de la frase esto se repite con cada letra gracias a for
+     }
+     }
+      if ( q => 1  ) { // esta otra condicional para ver las restricciones del cociente.
+     return [residue+1]//nos retorna la letra en residue aumentado en 1 .
 
-}else if { (alphabet2[i]==frase[i])
+  }         else {
 
-return i
-}
-}
-}
- var residue = (i + 33)%26-1
- return residue
- console.log(alphabet[residue]);
+       return position; //sino solo nos retorna la posicion de la letra.
+  }
+ }
+var rest =(position + 34)// La cual ingresaremos en una variable.
+console.log([rest])// nos muestra la letra en rest.
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 
 ### Ejercicio 1
+
+1) Mediante el comando prompt se le indica al usuario que ingrese una frase.
+
+2) Declaramos nuestra variable alphabet en la que ingresamos todas la letras del alfabeto.
+
+3) La  variable position  está relacionada con el ciclo for ya que nos dara la posición de cada letra que conforma la frase ingresada.
+
+4) La variable division1 nos indica el residuo de dividir la posicion de la letra mas los 33 espacios que deberia tener nuestra nueva letra entre la cantidad de elementos de la variable alphabet que en este caso es 52.
+
+5) Declaramos la variable division2 para ser mas específicos con cada elemento de una division en este caso "q " nos representa el cociente"
+
+6) Es necesario ingresar un while ya que esto necesitamos que se repita hasta que se culmine las letras de la frase ingresada.
+
+7) Aqui ingresamos un ciclo for que va a recorrer cada posicion de los elementos de la variable alphabet .
+
+8) Para lo que seguidamente ingresamos una condicional que le dira que debe hacer en este caso le indicamos que busque en toda la variable cada letra de frase ingresada .
+
+9) Si cumple la condicon nos retornará la posicion del elemento.
+
+10) Ingresamos otra condicional para ver si "q" que es el cociente es mayor que uno o no .
+
+11) Si cumple ello nos debera retrnar el residuo aumentado en uno.
+
+12) Si no nos retornará solomla posicion.
+
+13) La variable rest es creada que es la posicion siempre mas 34(33 de los espacios y uno de posicion que queremos de nuestro elemento)
+
+14) Indicamos que nos muestre rest.
